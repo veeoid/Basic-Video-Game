ed910679e8098bdf818ff1575f0b749d1481263d
Vismay Chaudhari <vismaychaudhari7@gmail.com>
2025-03-04 20:29:27 -0600

    added user prompt

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,12 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt("Maximum life for Player and Monster.", "100");
+
+let chosenMaxLife = parseInt(enteredValue);
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
