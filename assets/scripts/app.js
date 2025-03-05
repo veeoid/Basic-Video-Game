a99aeedd27e43500095c18c5972ba540e51ea1c8
Vismay Chaudhari <vismaychaudhari7@gmail.com>
2025-03-04 20:21:51 -0600

    reset game functionality added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,17 +6,39 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
+  const initalPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initalPlayerHealth;
+    setPlayerHealth(initalPlayerHealth);
+    alert("Bonus life just saved you!");
+  }
   if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert("Its a draw!");
   } else if (currentMonsterHealth <= 0) {
     alert("You Won!");
   } else if (currentPlayerHealth <= 0) {
     alert("Monster Won!");
+  }
+  if (
+    (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) ||
+    (currentMonsterHealth <= 0 && currentPlayerHealth > 0) ||
+    (currentMonsterHealth > 0 && currentPlayerHealth <= 0)
+  ) {
+    reset();
   }
 }
 
