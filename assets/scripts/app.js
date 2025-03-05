ee1b5c47cea28e27b3c0e864711d0504162ea11c
Vismay Chaudhari <vismaychaudhari7@gmail.com>
2025-03-04 19:45:34 -0600

    Added functionality to the game

---
+++ assets/scripts/app.js
@@ -0,0 +1,45 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("Its a draw!");
+  } else if (currentMonsterHealth <= 0) {
+    alert("You Won!");
+  } else if (currentPlayerHealth <= 0) {
+    alert("Monster Won!");
+  }
+}
+
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
+function healPlayerHandler() {
+  increasePlayerHealth(HEAL_VALUE);
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
